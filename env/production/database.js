d22ff2b67194f6d51a3d79a7b2cc57d84cca1ac4
Djordje Stojanovic <53429701+djo991@users.noreply.github.com>
2022-08-11 13:17:22 +0200

    Update database.js

--- env/production/database.js
+++ env/production/database.js
@@ -4,11 +4,11 @@ const parse = require('pg-connection-string').parse;
       connection: {
         client: 'postgres',
         connection: {
-          host: config.host,
-          port: config.port,
-          database: config.database,
-          user: config.user,
-          password: config.password,
+          host: env('DATABASE_HOST', '127.0.0.1'),
+          port: env.int('DATABASE_PORT', 5432),
+          database: env('DATABASE_NAME', 'strapi'),
+          user: env('DATABASE_USERNAME', 'strapi'),
+          password: env('DATABASE_PASSWORD', 'strapi'),
           ssl: {
             rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
           },
